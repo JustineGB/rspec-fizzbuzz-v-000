afdf5c089358c1113f3aaf1645b7d0b23c3fa607
Justine Bricknell <justinegayton@gmail.com>
2018-06-07 17:44:39 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,10 +3,15 @@ require_relative './spec_helper.rb'
 def fizzbuzz(input)
 end
 
-
-fizzbuzz(3) = "Fizz"
-
-Fizzbuzz(5) = "Buzz"
+if (theNumber is divisible by 3) and (theNumber is divisible by 5) then
+print "FizzBuzz"
+else if (theNumber is divisible by 3) then
+print "Fizz"
+else if (theNumber is divisible by 5) then
+print "Buzz"
+else /* theNumber is not divisible by 3 or 5 */
+print theNumber
+end if
 
 
 describe "fizzbuzz" do
