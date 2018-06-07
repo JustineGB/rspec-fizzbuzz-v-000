648f2a5c8b5dae1ad18f435a0b32ce9206ea4fae
Justine Bricknell <justinegayton@gmail.com>
2018-06-07 18:36:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,24 @@
 def fizzbuzz(int)
-if int % 3 == 0
+if int % 3 == 0 and int % 5 == 0
+  puts "FizzBuzz"
+elsif int % 5 == 0
+  puts "Buzz"
+elsif int % 3 == 0
+  puts "Fizz"
+else
+  puts nil
+  end
+end
+  
+  
   "Fizz"
+  
 elsif int % 5 == 0
   "Buzz"
 
 elsif int % 15 == 0
   "FizzBuzz"
-  
-end
-end
-end
-end
+
+fizz_3 = "Fizz"
+fizz_5 = "Buzz"
+fizz_15 = "FizzBuzz"
